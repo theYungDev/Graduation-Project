9e6cf5eeaa9b3aac4c382f412b246f43197d3f90
Zeyad <zeyadm12@outlook.com>
2024-02-03 21:40:11 +0200

    commit

--- script/index.js
+++ script/index.js
@@ -1,10 +1,14 @@
-let hover1 = document.getElementById("hover1");
-let card1 = document.getElementById("card1");
-hover1.addEventListener("mouseover", () => {
-  card1.style.top = "0";
-  card1.style.botom = "auto";
-});
-hover1.addEventListener("mouseout", () => {
-  card1.style.top = "auto";
-  card1.style.botom = "0";
-});
+let hover1 = document.getElementsByClassName("hover1");
+let card1 = document.getElementsByClassName("card1");
+let card2 = document.getElementsByClassName("card2");
+
+for (let index = 0; index < 4; index++) {
+  hover1[index].addEventListener("mouseover", () => {
+    card1[index].style.display = "none";
+    card2[index].style.display = "block";
+  });
+  hover1[index].addEventListener("mouseout", () => {
+    card1[index].style.display = "block";
+    card2[index].style.display = "none";
+  });
+}
