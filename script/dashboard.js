99da2475336239f106602889fe3fd2c9ec3d626d
Zeyad <zeyadm12@outlook.com>
2024-05-04 03:10:08 +0300

    dashboard courses finished

--- script/dashboard.js
+++
@@ -1,9 +0,0 @@
-const courseCards = document.getElementsByClassName("c-card");
-
-// console.log(typeof courseCards);
-
-Object.entries(courseCards).forEach((element) => {
-  element.addEventListener("mouseover", () => {
-    element.style;
-  });
-});
