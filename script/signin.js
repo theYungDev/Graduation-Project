17dd2248a3c7a16d0edc768c8b02afd87e6ce52f
Zeyad <zeyadm12@outlook.com>
2024-05-12 20:52:55 +0300

    signin forms modified

--- script/signin.js
+++ script/signin.js
@@ -1,7 +1,7 @@
 // signin buttons
 const myBtn1 = document.getElementById("mybtn1");
 const myBtn2 = document.getElementById("mybtn2");
-const formSelect = document.getElementById("frmslct");
+const formSelect = document.getElementById("frmSlct");
 
 myBtn1.onclick = () => {
   myBtn1.style.backgroundColor = "#00d084";
@@ -9,6 +9,8 @@ myBtn1.onclick = () => {
   myBtn2.style.color = "#192F59";
   myBtn2.style.backgroundColor = "transparent";
   formSelect.style.visibility = "hidden";
+  const studentForm = document.getElementById("student-form")
+  studentForm.style.display = "block"
 };
 myBtn2.onclick = () => {
   myBtn2.style.backgroundColor = "#00d084";
@@ -17,3 +19,15 @@ myBtn2.onclick = () => {
   myBtn1.style.color = "#192F59";
   formSelect.style.visibility = "visible";
 };
+// form accounts selector
+function accountTypeChanger(){
+  var selectedType = document.getElementById("frmSlct").value
+  console.log(selectedType);
+  document.getElementById("doctor-form").style.display = "none";
+  document.getElementById("admin-form").style.display = "none";
+  document.getElementById("accountant-form").style.display = "none";
+  document.getElementById(selectedType).style.display = "block";
+  // hide student form
+  const studentForm = document.getElementById("student-form")
+  studentForm.style.display = "none"
+}
